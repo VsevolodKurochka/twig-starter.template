b489aed19e22dbfff03cbd657d910c9741f20af9
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-08-25 16:01:50 +0300

    Fix

--- tasks/sass.js
+++ tasks/sass.js
@@ -8,6 +8,7 @@ import folders					from './folders';
 
 import sass 						from 'gulp-sass';
 import sassGlob 				from 'gulp-sass-glob';
+
 import csscomb 					from 'gulp-csscomb';
 import autoprefixer 		from 'gulp-autoprefixer';
 import cleanCSS					from 'gulp-clean-css';
@@ -19,18 +20,19 @@ import {reload} from './browserSync';
 // Task `sass`
 gulp.task('sass', () => gulp
 	.src(`${folders.src}/sass/**/*.+(sass|scss)`)
-	.pipe(sassGlob())
-	.pipe(sass({
-		outputStyle: 'expanded'
-	}).on('error', sass.logError))
-	.pipe(csscomb())
-	.pipe(autoprefixer({
-		browsers: ['last 15 versions'],
-		cascade: false
-	}))
-	.pipe(cleanCSS({compatibility: 'ie8'}))
-	.pipe(rename({suffix: '.min', prefix : ''}))
-	.pipe(gulp.dest(`${folders.build}/css`))
+		.pipe(sassGlob())
+		.pipe(sass({
+			outputStyle: 'expanded'
+		}).on('error', sass.logError))
+
+		.pipe(csscomb())
+		.pipe(autoprefixer({
+			browsers: ['last 15 versions'],
+			cascade: false
+		}))
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(rename({suffix: '.min', prefix : ''}))
+		.pipe(gulp.dest(`${folders.build}/css`))
 );
 
 
